62b6bc5f605c3f95b9c2396724e56c01d7f37e35
Timon Riemslagh <timon@timons-mbp.home>
2016-09-18 13:38:16 +0200

    styled header and grid

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-var albums = [];
 var apiCaller = new picasapi();
 var sliderphotosnumber = 3;
 
@@ -8,6 +7,8 @@ angular.module('myApp', [])
 })
 .controller('mainController', function($scope) {
 
+  $scope.albums = [];
+
   var setTheSlider = function(album) {
     $scope.sliderphotos = [];
     for(var t = 0; t < sliderphotosnumber; t++) {
@@ -16,9 +17,36 @@ angular.module('myApp', [])
       });
     }
 
-    console.log('test');
-
     $('.js-unslider').unslider();
+  };
+
+  var setEvent = function(object) {
+    $scope.upcomming = {};
+    $scope.$apply(function() {
+      $scope.upcomming.photo = object.photos[0].photo;
+      $scope.upcomming.desc = JSON.parse(object.photos[0].description);
+      var test = JSON.parse(object.photos[0].description);
+    });
+  };
+
+  var handleGrid = function() {
+    $scope.albumSubSets = [];
+    var subsets = Math.floor($scope.albums.length/4);
+    var subsetsrest = $scope.albums.length % 4;
+
+    for(var t = 0; t < subsets; t++) {
+      var albumSubSet = $scope.albums.slice((t*4), (t*4)+4);
+      $scope.$apply(function() {
+        $scope.albumSubSets.push(albumSubSet);
+      });
+    }
+
+    var finalSubSet = $scope.albums.slice($scope.albums.length - subsetsrest, $scope.albums.length);
+    $scope.$apply(function() {
+      $scope.albumSubSets.push(finalSubSet);
+    });
+
+    console.log($scope.albums);
   }
 
   var handlePhotos = function(data, object) {
@@ -30,17 +58,34 @@ angular.module('myApp', [])
     if(object.title == "vcSlideShow") {
       setTheSlider(object);
     }
+
+    if(object.title == "vcEvents") {
+      setEvent(object);
+    }
+
+    handleGrid();
   };
 
   var handleAlbums = function(data) {
     data.feed.entry.forEach(function(entry) {
+
       if(entry.title.$t.substr(0,2) === "vc") {
-        var album = {title: entry.title.$t, summary: entry.summary.$t, link: entry.link[0].href};
-        albums.push(album);
+        var summaryObj = "";
+        if(entry.summary.$t) {
+          summaryObj = JSON.parse(entry.summary.$t);
+        }
+
+        var album = {title: entry.title.$t, summary: summaryObj, link: entry.link[0].href};
+
+        if(album.title !== 'vcEvents' && album.title !== 'vcSlideShow') {
+          $scope.albums.push(album);
+        }
+
         apiCaller.getData(album.link, handlePhotos, album);
       }
     });
-  }
+
+  };
 
   apiCaller.getData('https://picasaweb.google.com/data/feed/api/user/krisrpr?alt=json', handleAlbums);
 
